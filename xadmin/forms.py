27feed797408cd9e4dcf1701fec0298f6f6ed11d
alexsilva <geniofuturo@gmail.com>
2022-11-14 16:05:38 -0300

    Just complementing the standard form validation.

--- xadmin/forms.py
+++ xadmin/forms.py
@@ -4,8 +4,10 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy, ugettext as _
 
-ERROR_MESSAGE = ugettext_lazy("Please enter the correct username and password "
-                              "for a staff account. Note that both fields are case-sensitive.")
+ERROR_MESSAGE = ugettext_lazy(
+	"Please enter the correct username and password "
+	"for a staff account. Note that both fields are case-sensitive."
+)
 
 
 class AdminAuthenticationForm(AuthenticationForm):
@@ -14,31 +16,13 @@ class AdminAuthenticationForm(AuthenticationForm):
 
 	"""
 	this_is_the_login_form = forms.BooleanField(
-		widget=forms.HiddenInput, initial=1,
-		error_messages={'required': ugettext_lazy("Please log in again, because your session has expired.")})
+		widget=forms.HiddenInput,
+		initial=1,
+		error_messages={
+			'required': ugettext_lazy("Please log in again, because your session has expired.")
+		})
 
-	def clean(self):
-		username = self.cleaned_data.get('username')
-		password = self.cleaned_data.get('password')
-		message = ERROR_MESSAGE
-
-		if username and password:
-			self.user_cache = authenticate(
-				username=username, password=password)
-			if self.user_cache is None:
-				if '@' in username:
-					User = get_user_model()
-					# Mistakenly entered e-mail address instead of username? Look it up.
-					try:
-						user = User.objects.get(email=username)
-					except (User.DoesNotExist, User.MultipleObjectsReturned):
-						# Nothing to do here, moving along.
-						pass
-					else:
-						if user.check_password(password):
-							message = _("Your e-mail address is not your username."
-							            " Try '%s' instead.") % user.username
-				raise forms.ValidationError(message)
-			elif not self.user_cache.is_active or not self.user_cache.is_staff:
-				raise forms.ValidationError(message)
-		return self.cleaned_data
+	def confirm_login_allowed(self, user):
+		if not user.is_staff:
+			raise self.get_invalid_login_error()
+		return super().confirm_login_allowed(user)
