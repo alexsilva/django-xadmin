12974e071f55af323a7d7275d28c2c3157bc7590
alexsilva <geniofuturo@gmail.com>
2023-06-12 17:39:47 -0300

    Call get_registry.

--- xadmin/filters.py
+++ xadmin/filters.py
@@ -357,7 +357,7 @@ class RelatedFieldSearchFilter(FieldFilter):
 		self.lookup_formats = {'in': '%%s__%s__in' % rel_name, 'exact': '%%s__%s__exact' % rel_name}
 		super(RelatedFieldSearchFilter, self).__init__(field, request, params, model, model_admin, field_path)
 
-		related_modeladmin = self.admin_view.admin_site._registry.get(other_model)
+		related_modeladmin = self.admin_view.admin_site.get_registry(other_model)
 		self.relfield_style = related_modeladmin.relfield_style
 
 		if hasattr(field, 'verbose_name'):
