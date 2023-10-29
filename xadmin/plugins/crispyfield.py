03428869a9a5e71a7b9fdbbd6ee42e18c964a788
alexsilva <geniofuturo@gmail.com>
2023-10-29 19:30:00 -0300

    Implemented form field configuration for crispy layout types.

--- xadmin/plugins/crispyfield.py
+++ xadmin/plugins/crispyfield.py
@@ -15,25 +15,39 @@ class CrispyFieldAdminPlugin(BaseAdminPlugin):
 			'attrs': {}
 		}
 	}
+	crispy_fields_overrides = {
+		BooleanField: SwitchCheckbox
+	}
 	"""
 	crispy_fields = {}
+	crispy_fields_overrides = {}
 
 	def init_request(self, *args, **kwargs):
-		return bool(len(self.crispy_fields) and TEMPLATE_PACK == "bootstrap4")
+		return bool(len(self.crispy_fields) or len(self.crispy_fields_overrides)
+		            and TEMPLATE_PACK == "bootstrap4")
 
-	def get_full_layout(self, layout, fields, **options):
+	def get_form_layout(self, layout):
 		for pointer in layout.get_field_names():
 			field_layout = layout
 			for position in pointer.positions:
-				field_layout = field_layout.fields[position]
-				for field_name in self.crispy_fields:
-					if isinstance(field_layout, LayoutObject) and field_name in field_layout.fields:
-						field_opts = self.crispy_fields[field_name]
-						field_layout.fields[field_layout.fields.index(field_name)] = (
-							crispy_field(field_name, **field_opts.get('attrs', {}))
+				layout_field_name = field_layout.fields[position]
+				if isinstance(layout_field_name, LayoutObject):
+					field_layout = layout_field_name
+				if isinstance(layout_field_name, str):
+					if field_opts := self.crispy_fields.get(layout_field_name):
+						field_layout.fields[field_layout.fields.index(layout_field_name)] = (
+							crispy_field(layout_field_name, **field_opts.get('attrs', {}))
 							if (crispy_field := field_opts['field']) and callable(crispy_field) else
 							crispy_field
 						)
+					elif form_obj := getattr(self.admin_view, "form_obj", None):
+						try:
+							form_field_class = type(form_obj.fields[layout_field_name])
+						except KeyError:
+							continue
+						if layout_field_class := self.crispy_fields_overrides.get(form_field_class):
+							layout_field_class = layout_field_class(layout_field_name) if callable(layout_field_class) else layout_field_class
+							field_layout.fields[field_layout.fields.index(layout_field_name)] = layout_field_class
 		return layout
 
 
