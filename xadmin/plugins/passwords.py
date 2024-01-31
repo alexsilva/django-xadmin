57d232b0ec225d64c7e014fa633b23181708d375
alexsilva <geniofuturo@gmail.com>
2024-01-31 00:06:09 -0300

    Fix icon font.

--- xadmin/plugins/passwords.py
+++ xadmin/plugins/passwords.py
@@ -60,7 +60,7 @@ class ResetLinkPlugin(BaseAdminPlugin):
 
 	def block_form_bottom(self, context, nodes):
 		reset_link = self.get_admin_url('xadmin_password_reset')
-		return '<div class="text-info" style="margin-top:15px;"><a href="%s"><i class="fal fa-lg fa-question-circle mr-2"></i>%s</a></div>' % (
+		return '<div class="text-info" style="margin-top:15px;"><a href="%s"><i class="far fa-lg fa-question-circle mr-2"></i>%s</a></div>' % (
 		reset_link, _('Forgotten your password or username?'))
 
 
